a3a946b79b4f6b31d6df49d4dd470568ae165134
tabiojisann <takumiezumi@icloud.com>
2020-08-07 13:13:32 +0900

    マイページ

--- laravel/resources/views/users/show.blade.php
+++ laravel/resources/views/users/show.blade.php
@@ -1,2 +1,199 @@
-成功
+@extends('app')
+
+@section('title', 'マイページ')
+
+@section('content')
+  @include('nav')
+    <div class="container mt-5 mb-5 " style="max-width: 100%;">
+      <div class="row">
+
+        <div class="col-5 offset-1">
+          <img src="https://picsum.photos/500/500" alt="">
+        </div>
+
+        <div class="col-4  ">
+
+          <div class="name pb-5">
+            <h1>{{ $user->name }}</h1>
+            <p>{{ $user->name }}</p>
+          </div>
+          <table class="table mt-5">
+
+          <thead>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+            <tr>
+              <th style="width:30%" >特技</th>
+              <th >寝ること、きついトレーニングをすること</th>
+            </tr>
+          </thead>
+          </table>
+        </div>
+      </div>
+    </div> 
+  @include('footer')
+@endsection
+
+
+
+
+
 
